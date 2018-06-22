2ce0b2a4bbb591976f1e1c782e34ef35904849f4
shikuLi <shiku_li@163.com>
2018-06-22 16:14:23 +0800

    修改upload页面生成的input名字排序

--- listen/web/js/main.js
+++ listen/web/js/main.js
@@ -66,6 +66,19 @@ $(function(){
 		var testBoxNum = $('div.testbox');
 		for(var i=0;i<testBoxNum.length;i++){
 			testBoxNum.eq(i).attr('value',i+1);
+            var lis = testBoxNum.eq(i).find("li");
+            lis.eq(0).find("input").attr("name","subjectList["+i+"].optionA");
+            lis.eq(1).find("input").attr("name","subjectList["+i+"].optionB");
+            lis.eq(2).find("input").attr("name","subjectList["+i+"].optionC");
+            lis.eq(3).find("input").attr("name","subjectList["+i+"].optionD");
+            lis.eq(4).find("select").attr("name","subjectList["+i+"].answer");
+       /* <input type="text" name="subjectList[0].sort"/>
+                <input type="text" name="subjectList[0].answer"/>
+                <input type="text" name="subjectList[0].optionA"/>
+                <input type="text" name="subjectList[0].optionB"/>
+                <input type="text" name="subjectList[0].optionC"/>
+                <input type="text" name="subjectList[0].optionD"/>
+                <input type="text" name="subjectList[0].analysis"/>*/
 			// text(i+1);
 		}
 	}
