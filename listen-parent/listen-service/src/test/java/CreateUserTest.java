bac62b503c5765cc32db33b8d1284f1654a62ea8
Exleryz <zyezhou@163.com>
2019-02-16 16:14:03 +0800

    测试专用

--- listen-parent/listen-service/src/test/java/CreateUserTest.java
+++ listen-parent/listen-service/src/test/java/CreateUserTest.java
@@ -8,7 +8,7 @@ public class CreateUserTest {
 
     @Test
     public void test1() {
-        String uuidSalt = UUID.randomUUID().toString().replace("-","");
+        String uuidSalt = UUID.randomUUID().toString().replace("-", "");
         System.out.println(uuidSalt);
         String password = DigestUtils.md5DigestAsHex(("1111" + uuidSalt).getBytes());
 //        d9d3a3d3e4174af68fdc3cbd720aa75e
@@ -20,5 +20,24 @@ public class CreateUserTest {
     public void test2() {
         Random r = new Random();
         System.out.println(r.nextInt(5));
+    }
+
+    @Test
+    public void test3() {
+        char[] answers = {'A', 'B', 'C', 'D', 'A'};
+        String answerStr = "A:1;B:2;A:3;C:2;D:1";
+
+        String[] kv = answerStr.split(";");
+        if (answers.length != kv.length) {
+            return;
+        }
+        Integer sumScore = 0;
+        for (int i = 0; i < answers.length; i++) {
+            String[] score = kv[i].split(":");
+            if (score[0].equals(answers[i] + "")) {
+                sumScore += Integer.parseInt(score[1]);
+            }
+        }
+        System.out.println(sumScore);
     }
 }
